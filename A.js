a31aeefccf7c8e0e24084185752de5e643c85cf4
Jonathan Bolo <jb.bolo@gmail.com>
2015-05-14 17:32:52 +1200

    initial animation

--- A.js
+++ A.js
@@ -1,10 +1,5 @@
-function A() {
-  this.height = 0;
-  this.width = 0;
-  this.radius = 0;
-  this.cx = 0;
-  this.cy = 0;
-  this.stroke = "black";
-  this.stroke_width = 3;
-  this.fill = "red";
-}
+$(document).ready(function() {
+  $("#creature").animate({
+      left: '470px'
+  });
+});
