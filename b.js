1bea35dee75bc3604b1bf29245fa68d3e93a615c
peterjacobson <peterjacobson.nz@gmail.com>
2015-05-14 17:17:16 +1200

    pass 2 tests, show b in world

--- b.js
+++ b.js
@@ -1,13 +1,12 @@
 function B() {
-	this.name = 'flim'
-	this.$shell = $('<circle id="' + this.name + '"">')
-  
-}
+	this.name = 'flim';
+	this.shell = '<div id="' + this.name + '""></div>';
+};
 
 B.prototype.start() { // {x: 10, y:12}
+	
 }
 
-B.prototype.moveTo(coords) { // {x: 10, y:12}
-	$('#' + this.name).animate()
-	
+B.prototype.animateTo(coords, speed) { // {x: 10, y:12}
+	$('#' + this.name).animate({top: coords.y + '%', left: coords.x + '%'}, speed)
 }
