f8a81dbd12dc3c71bbebb2079f78b8e996d44c97
peterjacobson <peterjacobson.nz@gmail.com>
2015-05-15 11:00:09 +1200

    handling edge case of new coords from beginning location with no previous location

--- b.js
+++ b.js
@@ -2,42 +2,62 @@ function B() {
 	this.name = 'flim';
 	this.shell = '<div id="' + this.name + '""></div>';
 	this.id = '#' + this.name;
-	this.lastCoords = this.getCoords();
+	this.lastCoords;
 };
 
+// game loop
 B.prototype.start = function() { // 
+	// this.lastCoords = this.getCoords();
 	setInterval(this.move.bind(this), 100)
 	// setInterval(this.poop.bind(this), 150)
 }
-		
-B.prototype.move = function() {
-	// console.log('move')
+
+//
+B.prototype.move = function() { // Abstract to helper?
+	console.log('move')
 	var lastCoords = this.lastCoords;
-	var currentCoords = this.getCoords();
+	var currentCoords = this.getCoords();  // function query (DOM)
 	var newCoords = this.newCoords({coords: {	now: 	currentCoords,
 																						prev: lastCoords},
 																	percentScreenMove: 3,
-																	bearingMaxVariation: Math.PI/2});
+																	bearingMaxVariation: Math.PI/2}); //function
 	var timeToReach = Math.random() * 5000 + 1000
-	this.animateTo(newCoords, timeToReach);
+	this.animateTo(newCoords, timeToReach); // function
 	this.lastCoords = currentCoords;
 }
 
-B.prototype.getCoords = function() {
+// DOM coupled
+B.prototype.getCoords = function() { // Abstract to helper?
 	var xNow = parseInt($(this.id).css('left'));
 	var yNow = parseInt($(this.id).css('top'));
 	return { x: xNow, y: yNow }
 }
 
-B.prototype.newCoords = function(args) {
+// Uncoupled
+B.prototype.newCoords = function(args) { // Abstract to helper?
+	var newCoords;
+	if (args.coords.prev === undefined) {
+		newCoords = locationBasedNextCoords(args)
+	} else if {
+		newCoords = vectorBasedNextCoords(args)
+	}
+	return newCoords
+}
+
+B.prototype.locationBasedNextCoords = function(args) {
+	return {x: xNew, y: yNew};
+}
+
+B.prototype.vectorBasedNextCoords = function(args) {
 	var currentBearing = Math.atan((args.coords.prev.x - args.coords.now.x)/
 													 	(args.coords.prev.y - args.coords.now.y));
 	var newBearing = currentBearing + Math.random() * args.bearingMaxVariation;
 	var xNew = args.percentScreenMove * Math.sin(newBearing);
 	var yNew = args.percentScreenMove * Math.cos(newBearing);
-	return {x: xNew, y: yNew}
+	return {x: xNew, y: yNew};
 }
 
-B.prototype.animateTo = function(coords, speed) { // {x: 10, y:12}
+// DOM coupled
+B.prototype.animateTo = function(coords, speed) { // Abstract to helper?
 	$(this.id).animate({top: coords.y + '%', left: coords.x + '%'}, speed)
 }
