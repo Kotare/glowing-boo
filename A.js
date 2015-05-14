3cc066e1da3dc017319e0ea80bfeb5937ce49cee
Jonathan Bolo <jb.bolo@gmail.com>
2015-05-14 21:14:17 +1200

    bouncing off walls

--- A.js
+++ A.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
-  $("#creature").animate({
-      left: '470px'
-  });
-});
+
+function A(name) {
+  this.name = name;
+  this.shell = '<div class="creature" id="' + this.name + '""></div>';
+};
+
+  A.prototype.start = function() { //
+    this.animateTo({x: 1, y: 1}, 1000)
+    this.animateTo({x: 95, y: 40}, 2000)
+    this.animateTo({x: 1, y: 1}, 1000)
+  }
+
+A.prototype.animateTo = function(coords, speed) { // {x: 10, y:12}
+  $('#' + this.name).animate({top: coords.y + '%', left: coords.x + '%'}, speed)
+}
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 $(document).ready( function() {
-	var b = new B()
-	var $bShell = $(b.shell)
-	$bShell.css({top: "20%", left: "20%"});
-	$bShell.appendTo('#world');
-	b.start()
-})
+	// var b = new B()
+	// var $bShell = $(b.shell)
+	// $bShell.css({top: "20%", left: "20%"});
+	// $bShell.appendTo('#world');
+	// b.start()
 
+  var a = new A('a');
+  var $aShell = $(a.shell)
+  $aShell.css({top: "20%", left: "20%"});
+  $aShell.appendTo('#world');
+  a.start();
+
+  var b = new A('b');
+  var $bShell = $(b.shell)
+  $bShell.css({top: "40%", left: "40%"});
+  $bShell.appendTo('#world');
+  b.start();
+
+})
