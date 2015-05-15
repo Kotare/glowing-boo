9e5cf3209e1ecae97c52ffe1923add743d8dd362
peterjacobson <peterjacobson.nz@gmail.com>
2015-05-15 14:26:09 +1200

    minor change

--- indexController.js
+++ indexController.js
@@ -1,10 +1,4 @@
 $(document).ready( function() {
-	var b = new B('flim')
-	var $bShell = $(b.shell)
 	var world = new World()
 	world()
-	$bShell.css({top: "20%", left: "20%"});
-	$bShell.appendTo('#world');
-	b.start()
-
 })
